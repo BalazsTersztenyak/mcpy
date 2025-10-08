c48c769d211517704ad13b7c29b02967355e025d
BalazsTersztenyak <tbalu912+github@gmail.com>
2025-10-08 14:11:48 +0200

    added test for world_data

--- tests/test_chunk_data.py
+++ tests/test_chunk_data.py
@@ -1,21 +1,31 @@
+import os
 from chunk_data import Chunk
 
 def test_chunk_creation():
-    chunk = Chunk(0, 0)
+    x: int = 0
+    z: int = 0
+    if os.path.exists(f"save_files/chunk_{x}_{z}.dat"):
+        os.remove(f"save_files/chunk_{x}_{z}.dat")
+    chunk = Chunk(x, z)
     assert type(chunk) is Chunk
-    assert chunk.x == 0
-    assert chunk.z == 0
-    assert chunk.get_block(0, 0, 0) == 1  # Default block type is stone (1)
-    chunk.unload_chunk()
+    assert chunk.x == x
+    assert chunk.z == z
+    assert chunk.get_block(0, 0, 0) == 0  # Default block type is air (0)
 
 def test_chunk_set_block():
-    chunk = Chunk(0, 0)
+    x: int = 0
+    z: int = 0
+    chunk = Chunk(x, z)
     chunk.set_block(0, 0, 0, 2)  # Change to grass block
     assert chunk.get_block(0, 0, 0) == 2
 
 def test_chunk_persistence():
-    chunk = Chunk(0, 0)
+    x: int = 0
+    z: int = 0
+    if os.path.exists(f"save_files/chunk_{x}_{z}.dat"):
+        os.remove(f"save_files/chunk_{x}_{z}.dat")
+    chunk = Chunk(x, z)
     chunk.set_block(0, 0, 0, 2)  # Change to grass block
     chunk.unload_chunk()
-    chunk2 = Chunk(0, 0)
+    chunk2 = Chunk(x, z)  # Reload chunk
     assert chunk2.get_block(0, 0, 0) == 2
