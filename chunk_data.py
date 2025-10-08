049a726520b78af2329d987a5956f4654418d3d4
BalazsTersztenyak <tbalu912+github@gmail.com>
2025-10-08 14:11:09 +0200

    removed temporary _load method

--- chunk_data.py
+++ chunk_data.py
@@ -9,12 +9,6 @@ class Chunk:
         self.SIZE = size
         self.blocks = {}
         self.load_chunk()
-    
-    def _load(self):
-        for x in range(self.SIZE):
-            for y in range(self.SIZE):
-                for z in range(self.SIZE):
-                    self.set_block(x, z, y, 1)  # Fill with stone (block ID 1)
 
     def _check_in_chunk(self, x, z, y):
         return 0 <= x < self.SIZE and 0 <= z < self.SIZE and 0 <= y < self.SIZE
@@ -23,7 +17,7 @@ class Chunk:
         try:
             self.blocks = pickle.load(open(f"save_files/chunk_{self.x}_{self.z}.dat", "rb"))
         except FileNotFoundError:
-            self._load()
+            self.blocks = {}
 
     def unload_chunk(self):
         pickle.dump((self.blocks), open(f"save_files/chunk_{self.x}_{self.z}.dat", "wb"))
